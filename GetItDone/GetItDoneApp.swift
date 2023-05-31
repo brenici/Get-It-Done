8adf0de2adf261c0cbf2b46dc3b19b009beb0530
Emilian Brenici <brenici@gmail.com>
2023-05-31 14:37:11 +0200

    Initial Commit

---
+++ GetItDone/GetItDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetItDoneApp.swift
+//  GetItDone
+//
+//  Created by Emilian Brenici on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GetItDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
