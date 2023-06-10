8cd2e92749de0a1ab4c284a3a6c0bfd83a68e226
Emilian Brenici <brenici@gmail.com>
2023-06-10 18:18:53 +0200

    Remove default UINavigationBar appearance.

--- GetItDone/Core/GetItDoneApp.swift
+++ GetItDone/Core/GetItDoneApp.swift
@@ -19,7 +19,17 @@ struct GetItDoneApp: App {
         WindowGroup {
             MainView()
                 .onAppear {
-                    // remove form/list background
+                    
+                    // prevent default NavigationBar appearance when scrolling up
+                    let appearance = UINavigationBarAppearance()
+                    appearance.backgroundEffect = nil
+                    appearance.backgroundColor = .clear
+                    appearance.backgroundImage = UIImage()
+                    appearance.shadowImage = UIImage()
+                    appearance.shadowColor = .clear
+                    UINavigationBar.appearance().standardAppearance = appearance
+                    
+                    // prevent form default background
                     UITableView.appearance().backgroundColor = .clear
                 }
         }
